13c719e5217393afe4bd3484d1ab4dda35bd7eec
fisyher <egret_ksm@hotmail.com>
2017-02-11 17:40:19 +0800

    Updated index.html and main.js
    Added placeholder empty element so that footer appears at the bottom
    Added a select element to select different number of pages per canvas
    Updated main.js to support the configurable number of pages per canvas option
    Clean up main.js to remove old unused code

--- public/js/main.js
+++ public/js/main.js
@@ -44,7 +44,7 @@ $(document).ready(function(){
 		charter2.setConfig({
 			scale: parseFloat( $('#SelectScaleFactor').val() ),
 			pageHeight: parseInt( $('#SelectPageHeight').val() ),
-			pagePerCanvas: 4
+			pagePerCanvas: parseInt( $('#SelectPagePerCanvas').val())
 		});
 
 		//
@@ -64,17 +64,17 @@ $(document).ready(function(){
 		//Add DOM manipulation code
 		$('#openFile').val('');
 		$("#chart_container").empty();
+		$('#placeholder').css('display', '');
 	});
 
 	$('#openFile').change(function(e){
 		//console.log(e);
-		
+
 		var f = e.target.files[0];
 		if(f){
 			var r = new FileReader();
 			r.onload = function(e) { 
-				var contents = e.target.result;
-				//console.log(contents);
+				var contents = e.target.result;				
 
 				//Parse contents and create dtx-object from it
 				//var dtx_parser = new DtxParser();
@@ -86,35 +86,32 @@ $(document).ready(function(){
 				if(ret){
 					dtxdataObject = dtxparserv2.getDtxDataObject();
 					console.log(dtxdataObject);
-					//console.log(JSON.stringify(dtxdataObject));
+					console.log(JSON.stringify(dtxdataObject));
 
 					lineMapper = new DtxChart.LinePositionMapper(dtxdataObject);
+					var estimatedDuration = lineMapper.estimateSongDuration();
+					console.log("Song is estimated to be " + estimatedDuration + " seconds long");
 					charter2.setDtxData(dtxdataObject, lineMapper);//
 					charter2.setConfig({
 						scale: parseFloat( $('#SelectScaleFactor').val() ),
 						pageHeight: parseInt( $('#SelectPageHeight').val() ),
-						pagePerCanvas: 4
+						pagePerCanvas: parseInt( $('#SelectPagePerCanvas').val())
 					});
 
 					//
 					var canvasConfigArray = charter2.canvasRequired();
 					console.log("Required canvas count: ",canvasConfigArray.length);
+					
+					//Clear chart before loading
+					$('#placeholder').css('display', 'none');					
+					$("#chart_container").empty();
 					//
 					createCanvasSheets(canvasConfigArray);
 
 					//canvasConfigArray[0].backgroundColor = "#000000";					
 					charter2.setCanvasArray(canvasConfigArray);
 					charter2.drawDTXChart();
-				}
-
-				// if(status){
-				// 	//Draw based on loaded dtxObject
-				// 	var scaleFactor = parseFloat($('#SelectScaleFactor').val());
-				// 	var pageHeight = Number($('#SelectPageHeight').val());
-				// 	currDtxObject = dtx_parser.dtxObject;
-				// 	//console.log(currDtxObject);
-				// 	//drawDtxChart(currDtxObject, plotter, scaleFactor, pageHeight);	
-				// }
+				}				
 			}
 			r.readAsText(f,encoding);
 		}
@@ -122,171 +119,3 @@ $(document).ready(function(){
 	});
 	
 });
-
-var drawDtxChart = function(dtxObject, plotter, scaleFactor, pageHeight){
-	plotter.clear({pageHeight:pageHeight});
-	plotter.drawChartInfo(dtxObject.chartInfo);
-
-	if(dtxObject.barGroups.length <= 0){
-		console.log('Nothing to chart so early exit');
-		return;
-	}
-
-	var startBpm = parseFloat(dtxObject.chartInfo.bpm);
-	var startBpmMarkerArray = dtxObject.barGroups[0].bpmMarkerArray;
-	var startBarLength = dtxObject.barGroups[0].barLength;
-
-	//
-	var positioner = new Xcharter.TimePixelPositioner(startBpm, startBpmMarkerArray, startBarLength,
-		{baseBpm: startBpm, scaleFactor: scaleFactor});
-	var currDrawBarLinesFlag = true;
-	for (var i = 0; i < dtxObject.barGroups.length; i++) {		
-		if(i > 0){
-			positioner.moveToNextBar(dtxObject.barGroups[i].bpmMarkerArray, dtxObject.barGroups[i].barLength);
-		}
-		var chipTimePixPos = positioner.getAbsoluteChipPosition(0);
-		var chipPixPos = plotter.plotBarNumber(i, chipTimePixPos);
-		//Draw barlines
-		plotter.plotBarLine("BarLine",chipTimePixPos);//At pos 0
-
-		//Draw barlines and quarterlines Conditionally!
-		//var showHideMarkerArray = dtxObject.barGroups[i].showHideLineMarkerArray;
-		// //Init all lines flag to be true
-		// var barLinesFlagsArray = [];
-		// var drawnLinesCount = Math.floor(positioner.currentBarLines/Xcharter.QUARTERLINECOUNT);
-		// for (var j = 0; j < drawnLinesCount; j++) {
-		// 	barLinesFlagsArray.push(true);
-		// };
-		// var lowerBound = 0;
-
-		//Draw QuarterLines
-		var quarterLinePosArray = positioner.currentBarQuarterLinePositions();
-		for (var j = 0; j < quarterLinePosArray.length; j++) {
-			plotter.plotBarLine("QuarterLine",quarterLinePosArray[j]);
-		};
-
-		//Draw BPM Markers
-		var currBpmMarkerArray = dtxObject.barGroups[i].bpmMarkerArray;
-		for (var j = 0; j < currBpmMarkerArray.length; j++) {
-			var bpmMarkTimePos = positioner.getAbsoluteChipPosition(currBpmMarkerArray[j].linePos);
-			plotter.plotBPMMarker(currBpmMarkerArray[j].bpm, bpmMarkTimePos);
-		};	
-
-		//Finally draw the chips for each lane
-		for (var j = 0; j < DtxParser.DtxBarLaneNames.length; j++) {
-			if(dtxObject.barGroups[i].hasOwnProperty(DtxParser.DtxBarLaneNames[j]) ){
-				var laneInBar = dtxObject.barGroups[i][ DtxParser.DtxBarLaneNames[j] ];
-
-				for (var k = 0; k < laneInBar.length; k++) {
-					chipTimePixPos = positioner.getAbsoluteChipPosition(laneInBar[k].linePos);
-					plotter.plotChip(DtxParser.DtxBarLaneNames[j], chipTimePixPos);
-				};
-			}
-		};
-
-
-	};
-
-	plotter.updateRender();
-
-}
-
-var drawTestChart = function(dtxObject, plotter){
-	
-	plotter.clear();
-
-	plotter.drawChartInfo({
-		title: "お米の美味しい炊き方、そしてお米を食べることによるその効果。",
-		level: "9.10",
-		bpm: "180",
-		noteCount: 1476
-	});
-	//Test positioner
-	var bpm = 180;
-
-	//bpmMarkerArray must be sorted in ascending linePos order
-	var bpmMarkerArray = [
-		 {bpm: 180, linePos: 0},
-		 {bpm: 100, linePos: 48}
-		 //{bpm: 150, linePos: 48}
-	];
-	var barlength = 1.0;
-
-	var positioner = new Xcharter.TimePixelPositioner(bpm, bpmMarkerArray,  1.0);
-	var bpmMarkTimePos = 0;
-	
-
-	for (var i = 0; i < 15; i++) {
-		var chipTimePixPos = positioner.getAbsoluteChipPosition(0);
-		var chipPixPos = plotter.plotBarNumber(i, chipTimePixPos);
-		chipPixPos = plotter.plotChip("LC", chipTimePixPos);
-		
-		//console.log(chipPixPos);
-
-		//Draw bar lines first
-		plotter.plotBarLine("BarLine",chipTimePixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(48);
-		//console.log(chipPixPos);
-		plotter.plotBarLine("QuarterLine",chipTimePixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(96);
-		plotter.plotBarLine("QuarterLine",chipTimePixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(144);
-		plotter.plotBarLine("QuarterLine",chipTimePixPos);
-
-		//Draw BPM Markers
-		for (var j = 0; j < bpmMarkerArray.length; j++) {
-			bpmMarkTimePos = positioner.getAbsoluteChipPosition(bpmMarkerArray[j].linePos);
-			plotter.plotBPMMarker(bpmMarkerArray[j].bpm, bpmMarkTimePos);
-		};		
-
-		//Draw chips
-		chipTimePixPos = positioner.getAbsoluteChipPosition(12);
-		chipPixPos = plotter.plotChip("HH", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(24);
-		chipPixPos = plotter.plotChip("LP", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(36);
-		chipPixPos = plotter.plotChip("SD", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(48);
-		chipPixPos = plotter.plotChip("HT", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(60);
-		chipPixPos = plotter.plotChip("BD", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(72);
-		chipPixPos = plotter.plotChip("LT", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(84);
-		chipPixPos = plotter.plotChip("FT", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(96);
-		chipPixPos = plotter.plotChip("RC", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		chipTimePixPos = positioner.getAbsoluteChipPosition(108);
-		chipPixPos = plotter.plotChip("RD", chipTimePixPos);
-		//console.log(chipPixPos);
-
-		bpmMarkerArray = [
-			 {bpm: 180, linePos: 0}
-			 //{bpm: 150, linePos: 48},
-			 //{bpm: 150, linePos: 48}
-		];
-
-		positioner.moveToNextBar(bpmMarkerArray, 1.0);
-	};
-
-	plotter.updateRender();
-};
